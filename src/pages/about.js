0067a285871359cf32b1e7d76e9ec21d80297f33
ext-Eva Taivalsaari <eva.taivalsaari@gmail.com>
2019-09-17 20:24:07 +0300

    Initial commit. Mostly just autogenerated stuff. Some empty pages added.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function About() {
+    return (
+        <Layout>
+            <div>jtn bask</div>
+        </Layout>
+    )
+}
